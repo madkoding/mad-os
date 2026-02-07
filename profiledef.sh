afdde8729e295efa6af0dd04383994263b5e308d
madkoding <madkoding@gmail.com>
2026-02-07 20:29:11 -0300

    feat(boot): add multiple boot modes support
    - Add BIOS boot modes: syslinux.mbr and syslinux.eltorito
    - Add UEFI boot modes: systemd-boot.esp and systemd-boot.eltorito
    - Update file permissions for .zlogin and toggle-demo-mode.sh

--- profiledef.sh
+++ profiledef.sh
@@ -8,8 +8,10 @@ iso_application="madOS - AI-Orchestrated Arch Linux"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux'
-           'uefi.systemd-boot')
+bootmodes=('bios.syslinux.mbr'
+            'bios.syslinux.eltorito'
+            'uefi-x64.systemd-boot.esp'
+            'uefi-x64.systemd-boot.eltorito')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
@@ -19,6 +21,7 @@ file_permissions=(
   ["/etc/sudoers.d/99-claude-nopasswd"]="0:0:440"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.zlogin"]="0:0:644"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
@@ -28,4 +31,5 @@ file_permissions=(
   ["/usr/local/bin/install-mados-gtk.py"]="0:0:755"
   ["/usr/local/bin/install-mados"]="0:0:755"
   ["/usr/local/bin/setup-claude-code.sh"]="0:0:755"
+  ["/usr/local/bin/toggle-demo-mode.sh"]="0:0:755"
 )
